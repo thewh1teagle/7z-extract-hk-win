697664c17e8ec23243b38f4a7244b7a10c694044
yakov <you@exampole.com>
2020-04-07 00:20:59 +0300

    removed unused libs, cleaner code.

--- current_file_extractor.py
+++ current_file_extractor.py
@@ -1,17 +1,8 @@
-import win32gui, time
-from win32con import PAGE_READWRITE, MEM_COMMIT, MEM_RESERVE, MEM_RELEASE, PROCESS_ALL_ACCESS, WM_GETTEXTLENGTH, WM_GETTEXT
-from commctrl import LVS_OWNERDATA, LVM_GETITEMCOUNT, LVM_GETNEXTITEM, LVNI_SELECTED
+import win32
+import win32gui
 import os
-import struct
-import ctypes
-import win32api
-import datetime
 import win32com.client as win32
-import win32ui
-import psutil
-import subprocess
-import time
-import urllib.parse
+
 
 
 def explorer_fileselection():
@@ -23,31 +14,26 @@ def explorer_fileselection():
         selected_files = shellwindows[window].Document.SelectedItems()
         for file in range(selected_files.Count):
             files.append(selected_files.Item(file).Path)
-        if len(files) == 1:
-            file = files[0]
-            return file
+    return files
 
 
-def try_to_get_file():
-    file = None
-    for _ in range(5):
-        file = explorer_fileselection()
-        print(file)
-        if file:
-            break
-    if file:
-        return file
-    
+def try_to_get_files():
+    for _ in range(3):
+        files = explorer_fileselection()
+        if len(files):
+            return files
+
 
 def extract_file(path):
     directory = os.path.dirname(os.path.abspath(path))
     os.chdir(directory)
-    command = r""" "C:/Program Files/7-Zip/7zG.exe" """  +  "e " +  path 
+    command = '"C:/Program Files/7-Zip/7zG.exe" e {}'.format(path)
     print(command)
     os.system(command)
 
 
 if __name__ == "__main__":
-    file = try_to_get_file()
-    if file:
-        extract_file(file)
+    if files := try_to_get_files():
+        for file in files:
+            extract_file(file) 
+        
